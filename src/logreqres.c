hanzoai/kv

commit 67b8a964d171d632e924ef99376b6d8481ae57a6

Updating reply_schema for sentinal commands (#12018)
Some sentinel subcommands are missing the reply_schema in the json file,
so add the proper reply_schema part in json file as sentinel replicas commands.

The schema validator was skipping coverage test for sentinel commands, this was initially
done just in order to focus on redis commands and leave sentinel coverage for later,
so this check is now removed.

sentinel commands that were missing reply schema:
* sentinel masters
* sentinel myid
* sentinel sentinels <master-name>
* sentinel slaves (deprecated)  <master-name>

utils/req-res-log-validator.py
@@ -334,9 +334,7 @@ if __name__ == '__main__':
     print("Hits per command:")
     for k, v in sorted(command_counter.items()):
         print(f"  {k}: {v}")
-    # We don't care about SENTINEL commands
-    not_hit = set(filter(lambda x: not x.startswith("sentinel"),
-                  set(docs.keys()) - set(command_counter.keys()) - set(IGNORED_COMMANDS)))
+    not_hit = set(set(docs.keys()) - set(command_counter.keys()) - set(IGNORED_COMMANDS))
     if not_hit:
         if args.verbose:
             print("WARNING! The following commands were not hit at all:")

commit 24215683162bc391017c5128ee0c23b701ed32ec

Minor change around the req-res validator, skip sentinel commands (#12083)
1. Check for missing schema only after the docs contain sentinel commands
2. The ignore-list in the C file contain only commands that cannot have a reply
  schema. The one in the py file is an extension of that list
3. Temp: skipsentinel commands don't have a schema or test coverage yet,
  add them to the py list

Solve CI error introduced by #12018

src/logreqres.c
@@ -186,7 +186,8 @@ size_t reqresAppendRequest(client *c) {
 
     /* Ignore commands that have streaming non-standard response */
     sds cmd = argv[0]->ptr;
-    if (!strcasecmp(cmd,"sync") ||
+    if (!strcasecmp(cmd,"debug") || /* because of DEBUG SEGFAULT */
+        !strcasecmp(cmd,"sync") ||
         !strcasecmp(cmd,"psync") ||
         !strcasecmp(cmd,"monitor") ||
         !strcasecmp(cmd,"subscribe") ||
@@ -194,11 +195,7 @@ size_t reqresAppendRequest(client *c) {
         !strcasecmp(cmd,"ssubscribe") ||
         !strcasecmp(cmd,"sunsubscribe") ||
         !strcasecmp(cmd,"psubscribe") ||
-        !strcasecmp(cmd,"punsubscribe") ||
-        !strcasecmp(cmd,"debug") ||
-        !strcasecmp(cmd,"pfdebug") ||
-        !strcasecmp(cmd,"lolwut") ||
-        (!strcasecmp(cmd,"sentinel") && argc > 1 && !strcasecmp(argv[1]->ptr,"debug")))
+        !strcasecmp(cmd,"punsubscribe"))
     {
         return 0;
     }

utils/req-res-log-validator.py
@@ -43,7 +43,9 @@ Future validations:
 1. Fail the script if one or more of the branches of the reply schema (e.g. oneOf, anyOf) was not hit.
 """
 
-IGNORED_COMMANDS = [
+IGNORED_COMMANDS = {
+    # Commands that don't work in a req-res manner (see logreqres.c)
+    "debug",  # because of DEBUG SEGFAULT
     "sync",
     "psync",
     "monitor",
@@ -53,11 +55,21 @@ IGNORED_COMMANDS = [
     "sunsubscribe",
     "psubscribe",
     "punsubscribe",
-    "debug",
+    # Commands to which we decided not write a reply schema
     "pfdebug",
     "lolwut",
-]
-
+    # TODO: write a reply schema for the following commands
+    "sentinel|debug",
+    "sentinel|info-cache",
+    "sentinel|pending-scripts",
+    "sentinel|reset",
+    "sentinel|simulate-failure",
+    "sentinel|help",
+    "sentinel|masters",
+    "sentinel|myid",
+    "sentinel|sentinels",
+    "sentinel|slaves",
+}
 
 class Request(object):
     """
@@ -216,6 +228,9 @@ def process_file(docs, path):
             if res.error or res.queued:
                 continue
 
+            if req.command in IGNORED_COMMANDS:
+                continue
+
             try:
                 jsonschema.validate(instance=res.json, schema=req.schema, cls=schema_validator)
             except (jsonschema.ValidationError, jsonschema.exceptions.SchemaError) as err:
@@ -286,16 +301,6 @@ if __name__ == '__main__':
 
     fetch_schemas(args.cli, args.port, redis_args, docs)
 
-    missing_schema = [k for k, v in docs.items()
-                      if "reply_schema" not in v and k not in IGNORED_COMMANDS]
-    if missing_schema:
-        print("WARNING! The following commands are missing a reply_schema:")
-        for k in sorted(missing_schema):
-            print(f"  {k}")
-        if args.fail_missing_reply_schemas:
-            print("ERROR! at least one command does not have a reply_schema")
-            sys.exit(1)
-
     # Fetch schemas from a sentinel
     print('Starting Redis sentinel')
 
@@ -306,6 +311,16 @@ if __name__ == '__main__':
     sentinel_args = [args.server, config_file, '--port', str(args.port), "--sentinel"]
     fetch_schemas(args.cli, args.port, sentinel_args, docs)
     os.unlink(config_file)
+
+    missing_schema = [k for k, v in docs.items()
+                      if "reply_schema" not in v and k not in IGNORED_COMMANDS]
+    if missing_schema:
+        print("WARNING! The following commands are missing a reply_schema:")
+        for k in sorted(missing_schema):
+            print(f"  {k}")
+        if args.fail_missing_reply_schemas:
+            print("ERROR! at least one command does not have a reply_schema")
+            sys.exit(1)
 
     start = time.time()
 