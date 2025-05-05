hanzoai/kv

commit 49e44e5316908dbc351eb5409e55876fe9931c22

Environment variable REDISCLI_AUTH to VALKEYCLI_AUTH conversion (#1995)
Signed-off-by: Hiranmoy Das Chowdhury <hiranmoy.das70@gmail.com>

src/valkey-cli.c
@@ -84,7 +84,8 @@
 #define CLI_HISTFILE_DEFAULT ".valkeycli_history"
 #define CLI_RCFILE_ENV "REDISCLI_RCFILE"
 #define CLI_RCFILE_DEFAULT ".valkeyclirc"
-#define CLI_AUTH_ENV "REDISCLI_AUTH"
+#define CLI_AUTH_ENV "VALKEYCLI_AUTH"
+#define OLD_CLI_AUTH_ENV "REDISCLI_AUTH"
 #define CLI_CLUSTER_YES_ENV "REDISCLI_CLUSTER_YES"
 
 #define CLUSTER_MANAGER_SLOTS 16384
@@ -2907,6 +2908,9 @@ static int parseOptions(int argc, char **argv) {
 static void parseEnv(void) {
     /* Set auth from env, but do not overwrite CLI arguments if passed */
     char *auth = getenv(CLI_AUTH_ENV);
+    if (auth != NULL) {
+        auth = getenv(OLD_CLI_AUTH_ENV);
+    }
     if (auth != NULL && config.conn_info.auth == NULL) {
         config.conn_info.auth = auth;
     }

commit 0176b8a504061fd1c615701c3eed1a7e6932352f

Fix VALKEYCLI_AUTH env variable fallback to REDISCLI_AUTH mistake (#2038)
Fix flipped-logic mistake introduced in #1995.

Signed-off-by: Hiranmoy Das Chowdhury <hiranmoy.das70@gmail.com>

src/valkey-cli.c
@@ -2908,7 +2908,7 @@ static int parseOptions(int argc, char **argv) {
 static void parseEnv(void) {
     /* Set auth from env, but do not overwrite CLI arguments if passed */
     char *auth = getenv(CLI_AUTH_ENV);
-    if (auth != NULL) {
+    if (auth == NULL) {
         auth = getenv(OLD_CLI_AUTH_ENV);
     }
     if (auth != NULL && config.conn_info.auth == NULL) {