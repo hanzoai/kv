cb50deec47a4ed3b15030370c743f454a27c0ead
Zach Kelling <z@zeekay.io>
2026-02-13 22:15:50 -0800

    chore: sync uncommitted changes

---
+++ deps/lua/test/.!85865!life.lua
@@ -0,0 +1,7 @@
+-- life.lua
+-- original by Dave Bollinger <DBollinger@compuserve.com> posted to lua-l
+-- modified to use ANSI terminal escape sequences
+-- modified to use for instead of while
+
+local write=io.write
+
